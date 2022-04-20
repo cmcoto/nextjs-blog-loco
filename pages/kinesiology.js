5424ca0f4e3665c0426a9aca1e3f27daeae6a0bc
Carlos Coto <cmcoto@gmail.com>
2022-04-20 02:50:36 +0200

    List of Kinesiology Methods

--- pages/kinesiology.js
+++ pages/kinesiology.js
@@ -32,9 +32,21 @@ export default function Coto() {
         
          <p>Kinesiology is a holistic method of complementary therapy that uses muscle monitoring, as a biofeedback loop, to search the body for inbalances that may cause disease. </p> 
          <p>If the human body is balanced, then the body is healthy. </p>
-         <p>Kinesiology is based on Western and Eastern healing methods.</p>
-          
+         <p>Kinesiology is based on Western  and Eastern Traditional Chinese healing methods.</p>
+         <p>With Kinesiology you may identify and balance health problems and disorders in the physical, mental and spiritual areas.</p>
+         <p>Your body <strong>knows why</strong> it is out of balance, and by gaining this balance back, then you will gain health.</p>
           <br />
+        <p>I have studied the following methods:</p>
+        <li>Touch For Health 1-4</li>
+        <li>Applied Physiology 1-5</li>
+        <li>Sport Kinesiology 1 with John Maguire</li>
+        <li>Simply The Brain with Jacque Mooney</li>
+        <li>Gui: Understanding and Transporting Negative Energy with Ron Wayman</li>
+        <li>With the help of Jacque Mooney, I have been able to connect Handlink Technique with Applied Physiology, so I may use the speed of Handlink whith the robust base that Applied Physiology has in healing.</li>
+        <br />
+        <strong>Since I'm still studying to have a Diploma, I am still not accepted by the Insurance Companies here in Switzerland. Therefore the cost per hour is: 45 CHF</strong>
+        <br />
+        <br />
     </section>
       </div>
     </Layout>
