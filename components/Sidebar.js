862ce9593f1e63a92319c016de1b003fe31ee94c
Carlos Coto <cmcoto@gmail.com>
2022-05-08 20:14:04 +0200

    Working on i18n, initial switch in menu for English and Deutsch

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { bubble as Menu } from 'react-burger-menu'
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 
 
 
@@ -14,13 +15,26 @@ class Sidebar extends React.Component {
 
   render () {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    const changeLanguage = (e) => {
+      const locale = e.target.value;
+      router.push();
+    };
+    
     return (
       <Menu>
         <a id="home" className="menu-item" href="/">HOME</a>
         <a id="about" className="menu-item" href="/about">ABOUT</a>
-        
+        <br />
         {/*<a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
         
+        <select
+            onChange={changeLanguage}
+            defaultValue={ "en" }
+            className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
+          >
+            <option className="text-black" value="en">EN</option>
+            <option className="text-black" value="fr">DEU</option>
+        </select>
 
       </Menu>
     );
