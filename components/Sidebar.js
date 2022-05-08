ad91ceb5ed102bf8f04c134bba4eb62eeb003393
Carlos Coto <cmcoto@gmail.com>
2022-05-09 00:46:56 +0200

    Added the selector function to add to path DE

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,43 +2,36 @@ import React from 'react';
 import { bubble as Menu } from 'react-burger-menu'
 import Link from 'next/link'
 import { useRouter } from 'next/router';
+import en from '../locales/en';
+import de from '../locales/de';
 
 
+export default function Sidebar() {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === 'en' ? en : de;
 
-class Sidebar extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
-    
-
-    
+  const changeLanguage = (e) => {
+    const locale = e.target.value;
+    router.push(router.pathname, router.asPath, { locale });
   }
 
-  render () {
-    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
-    const changeLanguage = (e) => {
-      const locale = e.target.value;
-      router.push();
-    };
+  return (
+    <Menu>
+    <a id="home" className="menu-item" href="/">HOME</a>
+    <a id="about" className="menu-item" href="/about">ABOUT</a>
+    <br />
+    {/*<a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
     
-    return (
-      <Menu>
-        <a id="home" className="menu-item" href="/">HOME</a>
-        <a id="about" className="menu-item" href="/about">ABOUT</a>
-        <br />
-        {/*<a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
-        
-        <select
-            onChange={changeLanguage}
-            defaultValue={ "en" }
-            className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
-          >
-            <option className="text-black" value="en">EN</option>
-            <option className="text-black" value="fr">DEU</option>
-        </select>
+    <select
+        onChange={changeLanguage}
+        defaultValue={ "en" }
+        className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
+      >
+        <option className="text-black" value="en">EN</option>
+        <option className="text-black" value="de">DE</option>
+    </select>
 
-      </Menu>
-    );
-  }
+  </Menu>
+  );
 }
-
-export default Sidebar;
